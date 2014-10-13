b29056d9b31e271fc9102e7edd546b9f46e0258c
TagliatL <maurer.paul@ymail.com>
2014-10-13 18:45:11 +0200

    niksamere les controles de dauphin

--- Assets/Controlable.cs
+++ Assets/Controlable.cs
@@ -4,23 +4,68 @@ using System.Collections;
 public class Controlable : MonoBehaviour {
 
 	public int speed;
+	public int rotationSpeed;
+	public int maxSpeed;
+	bool controller;
+	private string[] gamepadList;
+
 	protected void FixedUpdate () {
 
 		GetInput();
 	}
 
-	void GetInput() {
-		//Input Manette
-		transform.Rotate(new Vector3(0,0,Input.GetAxis("L_XAxis_1"))* Time.deltaTime * speed);
-
-		if (Input.GetButtonDown("A_1"))
-			rigidbody.AddForce(new Vector3(0, Input.GetAxis("L_XAxis_1"), 0) * 500);
-
-		//Input Clavier
-		transform.Rotate(new Vector3(0,0,Input.GetAxis("K_XAxis"))* Time.deltaTime * speed);
+	void Update() {
+		gamepadList = Input.GetJoystickNames();
 		
-		if (Input.GetButtonDown("Space"))
-			rigidbody.AddForce(new Vector3(0, Input.GetAxis("K_XAxis"), 0) * 500);	
+		if (gamepadList.Length > 0)
+		{
+			controller = true;
+		}
+		
+		if (gamepadList.Length == 0)
+		{
+			controller = false;
+		}
+	}
 
+	void GetInput() {
+
+		rigidbody2D.velocity = new Vector3(0, 0, 0);
+
+		if(controller == true) 
+		{
+			//Input Manette
+			transform.Rotate(new Vector3(0,0,Input.GetAxis("L_XAxis_1"))* Time.deltaTime * rotationSpeed);
+			rigidbody2D.AddForce(transform.right * speed);
+			
+			if (Input.GetButton("A_1"))
+			{
+				if(speed < maxSpeed)
+					speed+=2;
+			}
+			else
+			{
+				if(speed >=2)
+					speed-=2;
+			}
+		}
+
+		if(controller == false)
+		{
+			//Input Clavier
+			transform.Rotate(new Vector3(0,0,Input.GetAxis("K_XAxis"))* Time.deltaTime * rotationSpeed);
+			rigidbody2D.AddForce(transform.right * speed);
+			
+			if (Input.GetButton("Space"))
+			{
+				if(speed < maxSpeed)
+					speed+=2;
+			}
+			else
+			{
+				if(speed >=2)
+					speed-=2;
+			}
+		}
 	}
 }
