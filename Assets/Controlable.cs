4ad1c2b3f6318181b5e37bae038cd2eeb4df23cb
TagliatL <maurer.paul@ymail.com>
2014-10-13 16:48:49 +0200

    le début du game oklm

---
+++ Assets/Controlable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlable : MonoBehaviour {
+
+	public int speed;
+	protected void FixedUpdate () {
+
+		GetInput();
+	}
+
+	void GetInput() {
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("L_XAxis_1"))* Time.deltaTime * speed);
+
+		if (Input.GetButtonDown("A_1"))
+			rigidbody.AddForce(new Vector3(0, Input.GetAxis("L_XAxis_1"), 0) * 500);	
+	}
+}
