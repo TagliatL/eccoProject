27e5470902a45cd7a4f8488f561dd9896ccc2de3
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2014-10-13 17:20:10 +0200

    Contrôles clavier, holala la folie

--- Assets/Controlable.cs
+++ Assets/Controlable.cs
@@ -10,9 +10,17 @@ public class Controlable : MonoBehaviour {
 	}
 
 	void GetInput() {
+		//Input Manette
 		transform.Rotate(new Vector3(0,0,Input.GetAxis("L_XAxis_1"))* Time.deltaTime * speed);
 
 		if (Input.GetButtonDown("A_1"))
-			rigidbody.AddForce(new Vector3(0, Input.GetAxis("L_XAxis_1"), 0) * 500);	
+			rigidbody.AddForce(new Vector3(0, Input.GetAxis("L_XAxis_1"), 0) * 500);
+
+		//Input Clavier
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("K_XAxis"))* Time.deltaTime * speed);
+		
+		if (Input.GetButtonDown("Space"))
+			rigidbody.AddForce(new Vector3(0, Input.GetAxis("K_XAxis"), 0) * 500);	
+
 	}
 }
